e6c4fa13df8d4970c926dad25d9f69b3c8862136
Alec Chen <altqqftw@gmail.com>
2021-02-11 20:31:03 -0800

    add backend and mongodb connection

--- backend/handler.js
+++ backend/handler.js
@@ -1,18 +1,31 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const connectToDatabase = require("./db");
+const Note = require("./notes.model.js");
+require("dotenv").config({ path: "./variables.env" });
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.hello = (event, context, callback) => {
+  console.log("Hello World");
+  callback(null, "Hello World");
+};
+
+module.exports.create = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase().then(() => {
+    Note.create(JSON.parse(event.body))
+      .then((note) =>
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(note),
+        })
+      )
+      .catch((err) =>
+        callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { "Content-Type": "text/plain" },
+          body: "Could not create the note.",
+        })
+      );
+  });
 };
