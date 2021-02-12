2ec073d4a9f2b5c4ff3556296de679f793fde2b2
bing <bingmo55@gmail.com>
2021-02-11 21:56:47 -0800

    changed schema and api link

--- backend/user.model.js
+++ backend/user.model.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
-  id: String,
   name: String,
   email: String,
   year: String,
   major: String,
+  likes: [String],
 });
 module.exports = mongoose.model("User", UserSchema);
